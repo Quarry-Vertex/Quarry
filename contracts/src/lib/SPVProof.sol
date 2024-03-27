b1a206ab80f92aef76b08ca14c804c200c5b744b
CadeMichael <cade.lueker@gmail.com>
2024-03-27 12:39:57 -0700

    spv proof attempts

--- contracts/src/lib/SPVProof.sol
+++ contracts/src/lib/SPVProof.sol
@@ -2,8 +2,71 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "forge-std/console.sol";
 
 abstract contract SPVProof {
+    function bytes32ToString(bytes32 _bytes32) public returns (string memory) {
+        bytes memory bytesArray = new bytes(32);
+        for (uint256 i; i < 32; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
+    }
+
+    function reverseBytes32(bytes32 input) public pure returns (bytes32) {
+        bytes32 reversed;
+        for (uint i = 0; i < 32; i++) {
+            reversed |= (input[i] & 0xFF) << (31 - i) * 8;
+        }
+        return reversed;
+    }
+    /*
+    function doubleSha256(bytes memory data) public pure returns (bytes32) {
+        return sha256(abi.encodePacked(sha256(data)));
+    }
+
+    function spvProof1(bytes32[] memory proof, bytes32 merkleRootHash, bytes32 txHash, uint256 index) public returns (bool success) {
+        bytes32 hash = txHash;
+
+        for (uint256 i = 0; i < proof.length; i++) {
+            bytes32 proofHash = proof[i];
+
+            if (index % 2 == 0) {
+                hash = doubleSha256(abi.encodePacked(hash, proofHash));
+            } else {
+                hash = doubleSha256(abi.encodePacked(proofHash, hash));
+            }
+            console.log(bytes32ToString(hash));
+            index = index / 2;
+        }
+
+        return hash == merkleRootHash;
+    }
+    */
+
+    function doubleSha256(bytes memory data) public pure returns (bytes32) {
+        return sha256(abi.encodePacked(sha256(data)));
+    }
+
+    function spvProof1(bytes32[] memory proof, bytes32 merkleRootHash, bytes32 txHash, uint256 index) public returns (bool success) {
+        bytes32 hash = (txHash);
+
+        for (uint256 i = 0; i < proof.length; i++) {
+            bytes32 proofHash = proof[i];
+
+            if (index % 2 == 0) {
+                hash = (doubleSha256(abi.encodePacked(hash, proofHash)));
+            } else {
+                hash = (doubleSha256(abi.encodePacked(proofHash, hash)));
+            }
+            index = index >> 1;
+        }
+
+        return hash == merkleRootHash;
+    }
+
+
+
     /*
         Let's say we have the following Merkle tree for four transactions (A, B, C, D):
 
--- contracts/test/lib/SPVProof.t.sol
+++ contracts/test/lib/SPVProof.t.sol
@@ -43,27 +43,37 @@ contract SPVProofTest is Test {
         Test getting the correct merkle root from every node in the merkle tree
     */
 
+    function reverseBytes32(bytes32 input) public pure returns (bytes32) {
+        bytes32 reversed;
+        for (uint i = 0; i < 32; i++) {
+            reversed |= (input[i] & 0xFF) << (31 - i) * 8;
+        }
+        return reversed;
+    }
     // Starting at Transaction A
     function test_FromAPasses() public {
         // Example of a valid Merkle path for transaction A in the Merkle tree
-        bytes32[] memory merklePath = new bytes32[](3);
-        // create transaction hashes in merkle path
-        bytes32 txA = "A";
-        bytes32 txB = "B";
-        bytes32 txC = "C";
-        bytes32 txD = "D";
-        bytes32 txCD = sha256(abi.encodePacked(sha256(abi.encodePacked(txC, txD))));
-        bytes32 txAB = sha256(abi.encodePacked(sha256(abi.encodePacked(txA, txB))));
+        // bytes32[] memory merklePath = new bytes32[](3);
+        bytes32[] memory merklePath = new bytes32[](10);
 
-        // populate merkle path
-        merklePath[0] = txA; // curhash (hash of the transaction)
-        merklePath[1] = txB;
-        merklePath[2] = txCD;
+        // create transaction hashes in merkle path (all reversed)
+        merklePath[0] = reverseBytes32(hex"cb1a27fa39d05a7bfc0b44f8d1b67a92b659de95a8bbefbc688d41976f6159b8");
+        merklePath[1] = reverseBytes32(hex"4da6c1c53bd8f0b5f2b06f96fbee3f4023071154019c756ebcad030c7ce99b24");
+        merklePath[2] = reverseBytes32(hex"32141b5a77bb3bfe86efbd6c24bbb54213f0694abfd927d50d3fe6627c06b51d");
+        merklePath[3] = reverseBytes32(hex"a23a60b7f30a4c3e326fa9c8e821c2091043011890d404c2a67bbee074266af4");
+        merklePath[4] = reverseBytes32(hex"ae0df2f9f37d5549dab9a79f259090de9b7442543ca03f9da1a2d8e4b1701c22");
+        merklePath[5] = reverseBytes32(hex"20ed045b0d4dddca96f1a9e525be0feb09e25d4c8f2686778de7db4cd64e29d3");
+        merklePath[6] = reverseBytes32(hex"1c5a155d31616dd90704b83a923f383d80ca7be64feafd19bc1a14ffb6c5e703");
+        merklePath[7] = reverseBytes32(hex"57bc38d50a278e7173f7bf93a9dde7d27effdb4525f641a539482357f15a179a");
+        merklePath[8] = reverseBytes32(hex"4552b14fd9879a7da1768e093ea52997f0e079f74f0bd9709fa9a267b74748fa");
+        merklePath[9] = reverseBytes32(hex"2d841c9ed1ec16a8481cc1d99c8c05ab2c14d76580c02d51d6acfa7b800a30b8");
 
-        // get the root
-        bytes32 root = sha256(abi.encodePacked(sha256(abi.encodePacked(txAB, txCD))));
+        bytes32 root = hex"3bf0614fc39756da9bd31f28a0e9568e57893a28ee2be5777f163da2e330f8c2";
+        bytes32 reversedRoot = reverseBytes32(root);
+        bytes32 txHash = hex"0f2a733f000d577e2df099a7e010e752663916c6287046285d92a51a12ab25ba";
+        bytes32 reversedTxHash = reverseBytes32(txHash);
 
-        assertTrue(pool.spvProof(merklePath, root), "Valid SPV proof should pass");
+        assertTrue(pool.spvProof1(merklePath, reversedRoot, reversedTxHash, 471), "Valid SPV proof should pass");
     }
 
     // Starting at Transaction B
